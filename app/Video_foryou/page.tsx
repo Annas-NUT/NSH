a568a3960ce174b3bf7f4416d3e4a4600d3836a3
Annas-NUT <nut.annascheko@gmail.com>
2025-01-03 02:55:03 +0700

    SSS

--- app/Video_foryou/page.tsx
+++ app/Video_foryou/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
 
 function Video_foryou() {
@@ -8,16 +8,15 @@ function Video_foryou() {
 
   useEffect(() => {
     if (typeof window !== "undefined") {
-      // เริ่มต้นการใช้งาน localStorage หรือ window ที่ทำงานบน client-side
       setIsClient(true);
-      const isAuthenticated = localStorage.getItem('authenticated');
+      const isAuthenticated = localStorage.getItem("authenticated");
       if (!isAuthenticated) {
-        router.push('/'); // พาผู้ใช้ไปหน้า login
+        router.push("/"); // พาผู้ใช้ไปหน้า login
       }
     }
   }, [router]);
 
-  if (!isClient) return null; // ถ้ายังไม่ได้เป็น client ให้ไม่ render อะไร
+  if (!isClient) return null; // ไม่ render อะไรในฝั่ง server
 
   return (
     <main className="flex min-h-screen flex-col container mx-auto bg-black">
@@ -26,20 +25,13 @@ function Video_foryou() {
           <h1 className="text-center text-4xl font-extrabold text-pink-400 shadow-lg">
             Welcome to My Video
           </h1>
-          <video
-            controls
-            className="rounded-lg shadow-2xl w-auto h-full object-cover mt-8 border-4 border-pink-500"
-          >
+          <video controls className="rounded-lg shadow-2xl w-auto h-full object-cover mt-8 border-4 border-pink-500">
             <source src="/videos/my-word.mp4" type="video/mp4" />
             Your browser does not support the video tag.
           </video>
         </div>
         <div className="px-12 mt-6 w-full sm:w-auto">
-          <button
-            aria-label="Go back to home"
-            onClick={() => router.push('/')}
-            className="px-8 py-4 w-full sm:w-fit rounded-full bg-gradient-to-br from-pink-600 to-pink-400 hover:bg-gradient-to-br hover:from-pink-500 hover:to-pink-700 text-white font-bold border-2 border-pink-500 mt-4 lg:mt-0 transition duration-300 ease-in-out transform hover:scale-105 hover:shadow-2xl"
-          >
+          <button aria-label="Go back to home" onClick={() => router.push("/")} className="px-8 py-4 w-full sm:w-fit rounded-full bg-gradient-to-br from-pink-600 to-pink-400 hover:bg-gradient-to-br hover:from-pink-500 hover:to-pink-700 text-white font-bold border-2 border-pink-500 mt-4 lg:mt-0 transition duration-300 ease-in-out transform hover:scale-105 hover:shadow-2xl">
             Comeback
           </button>
         </div>
@@ -49,4 +41,3 @@ function Video_foryou() {
 }
 
 export default Video_foryou;
-
